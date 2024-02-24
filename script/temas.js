2adf2baf7238a72d038c5f3bd361850a05861037
Pedro Lyrio <pedrolyrio@ucl.br>
2024-02-24 11:43:26 -0300

    Feito navbar

---
+++ script/temas.js
@@ -0,0 +1,10 @@
+
+    function toggleDarkMode() {
+        document.body.classList.add('dark-mode');
+    document.body.classList.remove('light-mode');
+        }
+
+    function toggleLightMode() {
+        document.body.classList.add('light-mode');
+    document.body.classList.remove('dark-mode');
+        }
