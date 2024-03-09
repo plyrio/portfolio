90ad32a274800dde7560631836409157c09f695b
Pedro Lyrio <pedrolyrio@ucl.br>
2024-03-09 18:21:59 -0300

    Feito algumas correções no css e no bs5

--- script/temas.js
+++ script/temas.js
@@ -1,10 +1,32 @@
 
-    function toggleDarkMode() {
-        document.body.classList.add('dark-mode');
+// Mudar tema da pagina
+function toggleDarkMode() {
+    document.body.classList.add('dark-mode');
     document.body.classList.remove('light-mode');
-        }
+}
 
-    function toggleLightMode() {
-        document.body.classList.add('light-mode');
+function toggleLightMode() {
+    document.body.classList.add('light-mode');
     document.body.classList.remove('dark-mode');
+}
+
+
+// fazer a navbar ficar transparente
+window.onscroll = scroll;
+
+function scroll() {
+    var scrollTop = window.pageYOffset;
+    if (scrollTop > 30) {
+        try {
+            document.getElementById('antes').id = 'depois'
+        } catch (e) {
+            false
         }
+    } else {
+        try {
+            document.getElementById('depois').id = 'antes'
+        } catch (e) {
+            false
+        }
+    }
+}
