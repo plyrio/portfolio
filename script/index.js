40d764aebdcee7a03a670319ab36a06d9df22287
Pedro Lyrio <pedrolyrio@ucl.br>
2024-03-09 23:46:15 -0300

    Quebrando a cabeça com a parte dos cards

---
+++ script/index.js
@@ -0,0 +1,29 @@
+
+// Fazer o slider com cards
+var swiper = new Swiper(".mySwiper", {
+    zoom: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 15000,
+        disableOnInteraction: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
--- script/temas.js
+++ script/temas.js
@@ -30,3 +30,32 @@ function scroll() {
         }
     }
 }
+
+// sweeper
+
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
