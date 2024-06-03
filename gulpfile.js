926b0cd31cf6a8f985a7c8155b8a21fe79b33709
Pedro Lyrio <138774252+plyrio@users.noreply.github.com>
2024-06-03 11:33:41 +0000

    correção do gulp, colocando  type module no package para importação dinâmica

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,87 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer').default;
-const cleanCSS = require('gulp-clean-css');
-const imagemin = require('gulp-imagemin');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const browserSync = require('browser-sync').create();
-const del = require('del');
+import gulp from 'gulp';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css';
+import rename from 'gulp-rename';
+import uglify from 'gulp-uglify';
+import browserSync from 'browser-sync';
+import { deleteAsync } from 'del';
+import autoprefixer from 'gulp-autoprefixer';
+import imagemin from 'gulp-imagemin';
+
+const sass = gulpSass(dartSass);
+const bs = browserSync.create();
 
 const paths = {
     styles: {
-        src: 'src/assets/sass/**/*.scss',
-        dest: 'public/css'
-    },
-    scripts: {
-        src: 'src/assets/js/**/*.js',
-        dest: 'public/js'
-    },
-    images: {
-        src: 'src/assets/img/**/*.{jpg,jpeg,png,svg,gif}',
-        dest: 'public/img'
-    },
-    html: {
-        src: 'src/**/*.html'
-    }
-};
+            src: 'src/assets/sass/**/*.scss',
+                    dest: 'public/css'
+                        },
+                            scripts: {
+                                    src: 'src/assets/js/**/*.js',
+                                            dest: 'public/js'
+                                                },
+                                                    images: {
+                                                            src: 'src/assets/img/**/*.{jpg,jpeg,png,svg,gif}',
+                                                                    dest: 'public/img'
+                                                                        },
+                                                                            html: {
+                                                                                    src: 'src/**/*.html'
+                                                                                        }
+                                                                                        };
 
-// Limpar a pasta de destino
-function clean() {
-    return del(['public']);
-}
+                                                                                        // Limpar a pasta de destino
+                                                                                        export function clean() {
+                                                                                            return deleteAsync(['public']);
+                                                                                            }
 
-// Compilar SCSS para CSS
-function styles() {
-    return gulp.src(paths.styles.src)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(paths.styles.dest))
-        .pipe(browserSync.stream());
-}
+                                                                                            // Compilar SCSS para CSS
+                                                                                            export function styles() {
+                                                                                                return gulp.src(paths.styles.src)
+                                                                                                        .pipe(sass().on('error', sass.logError))
+                                                                                                                .pipe(autoprefixer())
+                                                                                                                        .pipe(cleanCSS())
+                                                                                                                                .pipe(rename({ suffix: '.min' }))
+                                                                                                                                        .pipe(gulp.dest(paths.styles.dest))
+                                                                                                                                                .pipe(bs.stream());
+                                                                                                                                                }
 
-// Minificar e renomear JavaScript
-function scripts() {
-    return gulp.src(paths.scripts.src)
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(paths.scripts.dest))
-        .pipe(browserSync.stream());
-}
+                                                                                                                                                // Minificar e renomear JavaScript
+                                                                                                                                                export function scripts() {
+                                                                                                                                                    return gulp.src(paths.scripts.src)
+                                                                                                                                                            .pipe(uglify())
+                                                                                                                                                                    .pipe(rename({ suffix: '.min' }))
+                                                                                                                                                                            .pipe(gulp.dest(paths.scripts.dest))
+                                                                                                                                                                                    .pipe(bs.stream());
+                                                                                                                                                                                    }
 
-// Otimizar imagens
-function images() {
-    return gulp.src(paths.images.src)
-        .pipe(imagemin())
-        .pipe(gulp.dest(paths.images.dest));
-}
+                                                                                                                                                                                    // Otimizar imagens
+                                                                                                                                                                                    export function images() {
+                                                                                                                                                                                        return gulp.src(paths.images.src)
+                                                                                                                                                                                                .pipe(imagemin())
+                                                                                                                                                                                                        .pipe(gulp.dest(paths.images.dest));
+                                                                                                                                                                                                        }
 
-// Iniciar um servidor de desenvolvimento e assistir a mudanças
-function serve() {
-    browserSync.init({
-        server: {
-            baseDir: 'src'
-        }
-    });
+                                                                                                                                                                                                        // Iniciar um servidor de desenvolvimento e assistir a mudanças
+                                                                                                                                                                                                        export function serve() {
+                                                                                                                                                                                                            bs.init({
+                                                                                                                                                                                                                    server: {
+                                                                                                                                                                                                                                baseDir: 'src'
+                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                            });
 
-    gulp.watch(paths.styles.src, styles);
-    gulp.watch(paths.scripts.src, scripts);
-    gulp.watch(paths.images.src, images);
-    gulp.watch(paths.html.src).on('change', browserSync.reload);
-}
+                                                                                                                                                                                                                                                gulp.watch(paths.styles.src, styles);
+                                                                                                                                                                                                                                                    gulp.watch(paths.scripts.src, scripts);
+                                                                                                                                                                                                                                                        gulp.watch(paths.images.src, images);
+                                                                                                                                                                                                                                                            gulp.watch(paths.html.src).on('change', bs.reload);
+                                                                                                                                                                                                                                                            }
 
-// Tarefa para construir o projeto
-const build = gulp.series(clean, gulp.parallel(styles, scripts, images));
+                                                                                                                                                                                                                                                            // Tarefa para construir o projeto
+                                                                                                                                                                                                                                                            export const build = gulp.series(clean, gulp.parallel(styles, scripts, images));
 
-// Tarefa padrão para iniciar o servidor e assistir a mudanças
-const watch = gulp.series(build, serve);
+                                                                                                                                                                                                                                                            // Tarefa padrão para iniciar o servidor e assistir a mudanças
+                                                                                                                                                                                                                                                            export const watch = gulp.series(build, serve);
 
-// Exportar tarefas
-module.exports.default = clean;
-module.exports.default = styles;
-module.exports.default = scripts;
-module.exports.default = images;
-module.exports.default = build;
-module.exports.default = watch;
+                                                                                                                                                                                                                                                            // Exportar tarefas padrão
+                                                                                                                                                                                                                                                            export default watch;
+                                                                                                                                                                                                                                                            
