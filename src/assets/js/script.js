946b0b633e65626a22d3f29f9ef7c4a4a48669ec
Pedro Lyrio <138774252+plyrio@users.noreply.github.com>
2024-06-16 12:07:23 +0000

    Correção borda e script

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,5 +1,4 @@
-import Typewriter from 'typewriter-effect';
-
+const Typewriter = require('typewriter-effect/dist/core');
 
 document.addEventListener("DOMContentLoaded", function() {
     const tpwriter = document.getElementById('typewriter');
@@ -22,50 +21,50 @@ document.addEventListener("DOMContentLoaded", function() {
             .pauseFor(1000)
             .start();
     }
-    });
 
+    function toggleTheme() {
+        const element = document.querySelector('#dark-light');
+        const body = document.body;
 
-function toggleTheme() {
-    const element = document.querySelector('#dark-light');
-    const body = document.body;
-
-    if (element.classList.contains('bi-moon-stars')) {
-        // Mudar para Dark
-        element.classList.remove('bi-moon-stars');
-        element.classList.add('bi-brightness-high-fill');
-        body.classList.remove('theme-light'); // Remove a classe do tema claro
-        body.classList.add('theme-dark'); // Adiciona a classe do tema escuro
-    } else if (element.classList.contains('bi-brightness-high-fill')) {
-        // Mudar para Light
-        element.classList.remove('bi-brightness-high-fill');
-        element.classList.add('bi-moon-stars');
-        body.classList.remove('theme-dark'); // Remove a classe do tema escuro
-        body.classList.add('theme-light'); // Adiciona a classe do tema claro
-    }
-}
-
-
-document.querySelector('#theme-toggle').addEventListener('click', toggleTheme);
-
-// fazer a navbar ficar transparente
-window.onscroll = scroll;
-const element = document.getElementById('#antes', '#depois');
-
-function scroll() {
-    var scrollTop = window.pageYOffset;
-    if (scrollTop > 30) {
-        try {
-            document.getElementById('antes').id = 'depois'
-            element.classList.add('border-bottom');
-        } catch (e) {
-            false
-        }
-    } else {
-        try {
-            document.getElementById('depois').id = 'antes'
-            element.classList.remove('border-bottom');
-        } catch (e) {
-            false
+        if (element.classList.contains('bi-moon-stars')) {
+            // Mudar para Dark
+            element.classList.remove('bi-moon-stars');
+            element.classList.add('bi-brightness-high-fill');
+            body.classList.remove('theme-light');
+            body.classList.add('theme-dark');
+        } else if (element.classList.contains('bi-brightness-high-fill')) {
+            // Mudar para Light
+            element.classList.remove('bi-brightness-high-fill');
+            element.classList.add('bi-moon-stars');
+            body.classList.remove('theme-dark');
+            body.classList.add('theme-light');
         }
     }
-}
+
+    const themeToggle = document.querySelector('#theme-toggle');
+    if (themeToggle) {
+        themeToggle.addEventListener('click', toggleTheme);
+    }
+
+    window.onscroll = scroll;
+    const element = document.getElementById('antes');
+
+    function scroll() {
+        var scrollTop = window.pageYOffset;
+        if (scrollTop > 30) {
+            try {
+                document.getElementById('antes').id = 'depois';
+                element.classList.add('border-bottom');
+            } catch (e) {
+                console.error(e);
+            }
+        } else {
+            try {
+                document.getElementById('depois').id = 'antes';
+                element.classList.remove('border-bottom');
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    }
+});
