df8d9697ae756ca257ac65c542bd24c69685ccf0
Pedro Lyrio <138774252+plyrio@users.noreply.github.com>
2024-06-20 19:12:35 +0000

    adição de estilos

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -16,17 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
         const element = document.querySelector("#dark-light");
         const body = document.body;
 
-        if (element.classList.contains("bi-brightness-high-fill")) {
+        if (element.classList.contains("bi-moon-stars")) {
             // Mudar para Dark
-            element.classList.remove("bi-brightness-high-fill");
-            element.classList.add("bi-moon-stars");
+            element.classList.remove("bi-moon-stars");
+            element.classList.add("bi-brightness-high-fill");
             
             body.classList.remove("theme-light");
             body.classList.add("theme-dark");
-        } else if (element.classList.contains("bi-moon-stars")) {
+        } else if (element.classList.contains("bi-brightness-high-fill")) {
             // Mudar para Light
-            element.classList.remove("bi-moon-stars");
-            element.classList.add("bi-brightness-high-fill");
+            element.classList.remove("bi-bightness-high-fill");
+            element.classList.add("bi-moon-stars");
             
             body.classList.remove("theme-dark");
             body.classList.add("theme-light");
