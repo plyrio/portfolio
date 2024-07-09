3f42d93a2a526b3fa4c69eb8492bf9292171fb5a
Pedro Lyrio <pedrolyrio@ucl.br>
2024-07-08 22:33:37 -0300

    Adicionar sistema do swiperjs

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -86,3 +86,28 @@ window.addEventListener("scroll", function () {
 
 
 });
+
+
+// Swiperjs
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+    clickable: true,
+      },
+    breakpoints: {
+        640: {
+        slidesPerView: 1,
+    spaceBetween: 20,
+        },
+    768: {
+        slidesPerView: 3,
+    spaceBetween: 40,
+        },
+    1024: {
+        slidesPerView: 3,
+    spaceBetween: 50,
+        },
+      },
+    });
