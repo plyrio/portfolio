26b4679dd71eb8965a3394a7a623062226a048b0
Pedro Lyrio <pedrolyrio@ucl.br>
2024-07-12 11:45:44 -0300

    criado os cards

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -102,12 +102,12 @@ window.addEventListener("scroll", function () {
     spaceBetween: 20,
         },
     768: {
-        slidesPerView: 3,
+        slidesPerView: 2,
     spaceBetween: 40,
         },
     1024: {
-        slidesPerView: 3,
-    spaceBetween: 50,
+        slidesPerView: 2,
+    spaceBetween: 80,
         },
       },
     });
