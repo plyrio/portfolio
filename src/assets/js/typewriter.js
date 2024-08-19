fc438fce86088a0f9656852a8bbf5e81609cede4
Pedro Lyrio <138774252+plyrio@users.noreply.github.com>
2024-08-19 22:18:31 +0000

    Troca do icone do js e ordem dos icones

--- src/assets/js/typewriter.js
+++ src/assets/js/typewriter.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
         new Typewriter(tpwriter, {
             autoStart: true,
             cursor: "",
-        }).typeString('Oi, eu sou Pedro <img  class="wave" src="https://res.cloudinary.com/dn5yfai0g/image/upload/v1723995570/hand-wave_mp4gm2.png" />').start();
+        }).typeString('Oi, eu sou Pedro <img  class="wave" src="https://res.cloudinary.com/dn5yfai0g/image/upload/v1723995570/hand-wave_mp4gm2.png" alt="hand-wave"/>').start();
     }
 });
