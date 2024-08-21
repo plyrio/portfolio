2a0e39dc63356144e6e7905244e10ee1291e7960
Pedro Lyrio <138774252+plyrio@users.noreply.github.com>
2024-08-21 22:32:09 +0000

    Inserir minha logo e favicon

--- src/assets/js/theme-toggle.js
+++ src/assets/js/theme-toggle.js
@@ -2,14 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const themeToggle = document.querySelector("#toggleTheme");
     const element = document.querySelector("#dark-light");
     const body = document.body;
-    const closebtn = document.querySelector("#btn-close");
+    const favicon = document.getElementById("favicon");
+    const logomark = document.getElementById("logomark");
+    const logomarkOff = document.getElementById("logomarkOff");
 
     function applyTheme(theme) {
         body.classList.toggle("theme-dark", theme === "dark");
         body.classList.toggle("theme-light", theme === "light");
         element.classList.toggle("icon-sun", theme === "dark");
         element.classList.toggle("icon-moon-star", theme === "light");
-        closebtn.classList.toggle("btn-close-white", theme === "dark");
+
+        favicon.href = theme === "dark" ? "https://res.cloudinary.com/dn5yfai0g/image/upload/v1724266397/4_yoxeqm.png" : "https://res.cloudinary.com/dn5yfai0g/image/upload/v1724266396/3_tc1gnn.png";
+        if (logomark) {
+        logomark.src = theme === "dark" ? "https://res.cloudinary.com/dn5yfai0g/image/upload/v1724270319/Pl_8_qfvnt4.png" : "https://res.cloudinary.com/dn5yfai0g/image/upload/v1724270319/Pl_9_ayrbyz.png";
+        }
+        if (logomarkOff) {
+        logomarkOff.src = theme === "dark" ? "https://res.cloudinary.com/dn5yfai0g/image/upload/v1724270319/Pl_8_qfvnt4.png" : "https://res.cloudinary.com/dn5yfai0g/image/upload/v1724270319/Pl_9_ayrbyz.png";
+            }
+    
     }
 
     function toggleTheme() {
